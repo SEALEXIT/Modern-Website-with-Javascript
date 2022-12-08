9c16f671221b2a89752bc0aab5c82a5ae885f487
ALEX RUGARA <118119828+SEALEXIT@users.noreply.github.com>
2022-12-08 11:22:27 +0300

    My First Commit .
    In this commit just added some basic HTML and CSS design of the application and minimal JavaScript functionality such as smooth scrolling  event handlers  and a bit of mouseover event handler on the h1 tittle.

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(function (btn) {
+  btn.addEventListener('click', openModal);
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+//Smooth Scrolling.
+const btnscrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.getElementById('section--1');
+btnscrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+//page navigation using nav Links at the header section.
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+const h1 = document.querySelector('h1');
+//going down :child
+console.log(h1.querySelectorall());
+/////////////////////////////
+//////////////////////////////////
+///////////////////////////
